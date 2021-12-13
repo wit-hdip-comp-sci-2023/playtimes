47cd8749b3e8759e0528e5dff4326051ab3122ec
Eamonn de Leastar <edeleastar@wit.ie>
2021-12-13 08:49:49 +0000

    updates to 05

--- playtime-0.5.0/src/models/db.js
+++ playtime-0.5.0/src/models/db.js
@@ -2,14 +2,14 @@
 // import { playlistMemStore } from "./mem/playlist-mem-store.js";
 // import { trackMemStore } from "./mem/track-mem-store.js";
 
-// import { userJsonStore } from "./json/user-json-store.js";
-// import { playlistJsonStore } from "./json/playlist-json-store.js";
-// import { trackJsonStore } from "./json/track-json-store.js";
+import { userJsonStore } from "./json/user-json-store.js";
+import { playlistJsonStore } from "./json/playlist-json-store.js";
+import { trackJsonStore } from "./json/track-json-store.js";
 
-import { userMongoStore } from "./mongo/user-mongo-store.js";
-import { playlistMongoStore } from "./mongo/playlist-mongo-store.js";
-import { initMongo } from "./mongo/init.js";
-import { trackMongoStore } from "./mongo/track-mongo-store.js";
+// import { userMongoStore } from "./mongo/user-mongo-store.js";
+// import { playlistMongoStore } from "./mongo/playlist-mongo-store.js";
+// import { initMongo } from "./mongo/init.js";
+// import { trackMongoStore } from "./mongo/track-mongo-store.js";
 
 export const db = {
   userStore: null,
@@ -30,10 +30,10 @@ export const db = {
   },
 
   init() {
-    this.userStore = userMongoStore;
-    this.playlistStore = playlistMongoStore;
-    this.trackStore = trackMongoStore;
-    initMongo();
-    this.seed();
+    this.userStore = userJsonStore;
+    this.playlistStore = playlistJsonStore;
+    this.trackStore = trackJsonStore;
+    // initMongo();
+    // this.seed();
   },
 };
