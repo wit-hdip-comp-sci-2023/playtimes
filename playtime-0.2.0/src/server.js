2c716446bf93b48b1b8d0bed589f94e26d4f36f2
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-31 08:15:55 +0000

    recover port from env

--- playtime-0.2.0/src/server.js
+++ playtime-0.2.0/src/server.js
@@ -13,8 +13,7 @@ const __dirname = path.dirname(__filename);
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
   });
 
   await server.register(Vision);
