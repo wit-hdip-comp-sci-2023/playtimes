1e4ff6cf7e92d2c2d46794c856a68ce81942b24e
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-01 12:53:22 +0000

    update

--- playtime-0.10.0/src/models/db.js
+++ playtime-0.10.0/src/models/db.js
@@ -4,10 +4,10 @@ import { trackMemStore } from "./mem/track-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
 import { playlistJsonStore } from "./json/playlist-json-store.js";
 import { trackJsonStore } from "./json/track-json-store.js";
-import { userMongoStore } from "./mongo/user-mongo-store.js";
-import { playlistMongoStore } from "./mongo/playlist-mongo-store.js";
-import { trackMongoStore } from "./mongo/track-mongo-store.js";
-import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "../controllers/mongo/user-mongo-store.js";
+import { playlistMongoStore } from "../controllers/mongo/playlist-mongo-store.js";
+import { trackMongoStore } from "../controllers/mongo/track-mongo-store.js";
+import { connectMongo } from "../controllers/mongo/connect.js";
 
 export const db = {
   userStore: null,
